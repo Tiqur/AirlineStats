ce970f9777ee65fb99e5bbeb9b8766b1fe291d3e
Tiqur <Tiqur7@gmail.com>
2021-01-17 01:20:45 -0800

    Ability to get all airline codes

---
+++ app.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const https = require("http");
+const app = express();
+const port = 3000;
+
+function getAirlineCodes() {
+  return new Promise(resolve => {
+    https.get('http://www.airportcodes.org/', resp => {
+      let data = '';
+
+      resp.on('data', chunk => {
+        data += chunk;
+      })
+
+      resp.on('end', () => {
+        let lines = data.split('\n');
+        lines = lines.map(line => line.substring(line.indexOf('(')+1, line.indexOf(')')));
+        lines = lines.filter(line => line.length == 3);
+        resolve(lines);
+      })
+    })
+  }) 
+}
+
+(async () => {
+  const airlineCodes = await getAirlineCodes();
+  app.listen(port, () => {
+    console.log(`Listening at http://localhost${port}`)
+  })
+})()
